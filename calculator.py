a8239441d082d921c60c60e09216f32aa9078c23
ayooesther <47854723+ayooesther@users.noreply.github.com>
2019-04-23 15:28:15 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+
+def subtract(a,b):
+	c=a+b
+	print(c)
+
+def multiply(a,b):
+	c=a*b
+	print(c)
+
+def division(a,b):
+	c=a/b
+	print(c)
+
+
+def modulus(a,b):
+	c=a%b
+	print(c)
+
+def exponent(a,b):
+	c=a**b
+	print(c)
+
+def whole_division(a,b):
+	c=a//b
+	print(c)
+
+def percentage(a,b):
+	c=a / b
+	e=c/100
+	print(c)
+
